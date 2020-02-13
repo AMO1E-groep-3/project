7c37a74143e233c6d9985d9e037eb328f7da93d0
Biko <Bikooschouten@gmail.com>
2020-02-13 23:23:13 +0100

    Add receipt view if refresh is pressed in 'PenningMeesterForm.cs'

--- barSysteem/barSysteem/penningMeesterForm.Designer.cs
+++ barSysteem/barSysteem/penningMeesterForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace barSysteem
 {
-    partial class penningMeesterForm
+    partial class PenningMeesterForm
     {
         /// <summary>
         /// Required designer variable.
@@ -32,9 +32,10 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -45,21 +46,25 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(37, 27);
+            this.dataGridView1.Location = new System.Drawing.Point(99, 64);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(343, 407);
+            this.dataGridView1.RowHeadersWidth = 102;
+            this.dataGridView1.Size = new System.Drawing.Size(915, 971);
             this.dataGridView1.TabIndex = 0;
             // 
             // Column1
             // 
             this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 12;
             this.Column1.Name = "Column1";
             // 
             // Column2
             // 
             this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 12;
             this.Column2.Name = "Column2";
             // 
             // dataGridView2
@@ -67,43 +72,58 @@
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2});
-            this.dataGridView2.Location = new System.Drawing.Point(397, 27);
+            this.dataGridViewTextBoxColumn2,
+            this.Column3});
+            this.dataGridView2.Location = new System.Drawing.Point(1059, 64);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(343, 407);
+            this.dataGridView2.RowHeadersWidth = 102;
+            this.dataGridView2.Size = new System.Drawing.Size(1461, 1191);
             this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(2521, 39);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 141);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "⟳";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // dataGridViewTextBoxColumn1
             // 
             this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Products";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 12;
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             // 
             // dataGridViewTextBoxColumn2
             // 
             this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dataGridViewTextBoxColumn2.HeaderText = "Column2";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Price";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 12;
             this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
             // 
-            // button1
+            // Column3
             // 
-            this.button1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(746, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 59);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "⟳";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Column3.HeaderText = "Date";
+            this.Column3.MinimumWidth = 12;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 250;
             // 
-            // penningMeesterForm
+            // PenningMeesterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(823, 468);
+            this.ClientSize = new System.Drawing.Size(2711, 1381);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "penningMeesterForm";
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "PenningMeesterForm";
             this.Text = "penningMeesterForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
@@ -117,8 +137,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
