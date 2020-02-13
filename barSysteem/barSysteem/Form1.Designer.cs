0dfecf5e73a154f0677f5fc2df4314eab852025e
Matthias <matthiaskieboom@gmail.com>
2020-02-13 08:45:36 +0100

    Button verplaatst

--- barSysteem/barSysteem/Form1.Designer.cs
+++ barSysteem/barSysteem/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(224, 166);
+            this.button1.Location = new System.Drawing.Point(464, 271);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(230, 121);
             this.button1.TabIndex = 0;
             this.button1.Text = "testbutton";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
