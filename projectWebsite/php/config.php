b90913ec70a9c36139d7d05672709e1c61e91957
Matthias <matthiaskieboom@gmail.com>
2020-02-21 16:11:00 +0100

    website
    website saldo opwaarderen gefixd

---
+++ projectWebsite/php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost = "localhost";
+$dbName = "project";
+$dbUser = "root";
+$dbPass = "";
+
+$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+
+?>
---
+++ projectWebsite/php/detail.php
@@ -0,0 +1,34 @@
+<?php   
+require('../header.php');
+require('config.php');
+
+$code = $_POST['code'];
+$saldo = $_POST['saldo'];
+
+
+$sql = "SELECT * FROM accounts WHERE code = :code";
+$query = $db->prepare($sql);
+$query->execute([
+':code' => $code
+]);
+
+$account = $query->fetch(PDO::FETCH_ASSOC);
+$id = $account['id'];
+$oldSaldo = $account['saldo'];
+$newSaldo = $oldSaldo + $saldo;
+
+if(empty($code) || empty($saldo)){
+    header("Location: ../index.php");
+}
+else{
+    $sql = "UPDATE `accounts` SET `saldo` = $newSaldo WHERE `accounts`.`id` = $id";
+    $query = $db->query($sql);
+}
+?>
+
+<h1>Hey, <?= $account['name']; ?></h1>
+<h3>Je saldo is opgewaardeerd!</h3>
+
+
+
+
